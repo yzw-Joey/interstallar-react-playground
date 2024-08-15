1049a5e32abc778a01a66fcbe6468a8b7ad24399
yangzhiwen <yangzhiwen@tanyue.cc>
2024-08-15 14:52:49 +0800

    feat: add ui

--- src/ReactPlayground/template/main.tsx
+++ src/ReactPlayground/template/main.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import dayjs from "dayjs";
+import { Calendar } from "ui";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Calendar value={dayjs()} />
+);
